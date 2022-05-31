7d37a2740d6621e1109d8f248c419f7e26a64109
nazmul162001 <nazmulprime162001@gmail.com>
2022-06-01 00:08:20 +0600

    completed api created

--- models/todoSchema.js
+++ models/todoSchema.js
@@ -1,16 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const todoSchema = mongoose.Schema({
   todoTask: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
 const Todo = mongoose.model('Todo', todoSchema);
 export default Todo;
