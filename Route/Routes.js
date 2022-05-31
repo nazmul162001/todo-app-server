d14071bfb584adbb8060bab60feffe49c37bf0c3
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-31 15:33:32 +0600

    update get & update api

--- Route/Routes.js
+++ Route/Routes.js
@@ -1,56 +1,52 @@
-import express from "express";
-import Todo from '../models/todoSchema.js'
+import express from 'express';
+import Todo from '../models/todoSchema.js';
 const route = express.Router();
 
 // create new todo
-route.post('/', async(req,res)=> {
-  try{
+route.post('/', async (req, res) => {
+  try {
     const addTodo = await Todo(req.body).save();
     res.send(addTodo);
+  } catch (error) {
+    res.send({ error: error.message });
   }
-  catch(error){
-    res.send({error: error.message})
-  }
-})
+});
 
 // get todo
-route.get('/', async(req,res)=> {
-  try{
-    const getTodo = await Todo.findOne();
+route.get('/', async (req, res) => {
+  try {
+    const getTodo = await Todo.find();
     res.send(getTodo);
+  } catch (error) {
+    res.send({ error: error.message });
   }
-  catch(error){
-    res.send({error:error.message})
-  }
-})
+});
 
 // delete todo
-route.delete('/:id', async(req,res)=> {
-  try{
+route.delete('/:id', async (req, res) => {
+  try {
     const deleteTodo = await Todo.findByIdAndDelete(req.params.id);
-    res.send(deleteTodo)
+    res.send(deleteTodo);
+  } catch (error) {
+    res.send({ error: error.message });
   }
-  catch(error){
-    res.send({error: error.message})
-  }
-})
+});
 
 // update todo
-route.put('/id', async(req,res)=> {
-  try{
+route.put('/:id', async (req, res) => {
+  try {
     const updateTodo = await Todo.updateOne(
-      {_id: req.params.id},
+      { _id: req.params.id },
       {
         $set: {
-          todoTask: req.body.todoTask
-        }
+          todoTask: req.body.todoTask,
+        },
       }
-    )
+    );
     res.send(updateTodo);
+  } catch (error) {
+    res.send({ error: error.message });
   }
-  catch(error){
-    res.send({error: error.message})
-  }
-})
+});
 
 export default route;
