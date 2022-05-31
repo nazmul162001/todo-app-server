446eca9375c520b46b80656bd35b6a7a8afeb0ed
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-31 15:08:27 +0600

    create api for create new todo

--- Route/Routes.js
+++ Route/Routes.js
@@ -2,7 +2,16 @@ import express from "express";
 import Todo from '../models/todoSchema.js'
 const route = express.Router();
 
-
+// create new todo
+route.post('/', async(req,res)=> {
+  try{
+    const addTodo = await Todo(req.body).save();
+    res.send(addTodo);
+  }
+  catch(error){
+    res.send({error: error.message})
+  }
+})
 
 
 
